af8c4c47f29c86153fe29c5d108df8b7f06e07fa
wang5660 <hamiltonlwang12@gmail.com>
2024-02-05 12:19:28 -0500

    Change keepass osascript

--- .config/sketchybar/plugins/keepass.sh
+++ .config/sketchybar/plugins/keepass.sh
@@ -2,6 +2,6 @@
 
 osascript -e 'tell application "System Events" to tell process "KeePassXC"
     click menu bar item 1 of menu bar 2
-    click menu item "Lock All Databases" of menu 1 of menu bar item 1 of menu bar 2
+    click menu item 2 of menu 1 of menu bar item 1 of menu bar 2
 end tell'
 
